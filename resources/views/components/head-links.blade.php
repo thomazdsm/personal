018e1dc7e79d1b0e4271de12d3f5e9c62772c1dd
Thomaz Machado <thomazdsmachado@gmail.com>
2024-04-05 19:29:09 -0300

    first commit

---
+++ resources/views/components/head-links.blade.php
@@ -0,0 +1,27 @@
+<!-- Favicon icon-->
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicon/apple-touch-icon.png') }}" />
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon/favicon-32x32.png') }}" />
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon/favicon-16x16.png') }}" />
+<link rel="manifest" href="{{ asset('images/favicon/site.webmanifest') }}" />
+<link rel="mask-icon" href="{{ asset('images/favicon/block-safari-pinned-tab.svg') }}" color="#8b3dff" />
+<link rel="shortcut icon" href="{{ asset('images/favicon/favicon.ico') }}" />
+<meta name="msapplication-TileColor" content="#8b3dff" />
+<meta name="msapplication-config" content="{{ asset('images/favicon/tile.xml') }}" />
+
+<!-- Color modes -->
+<script src="{{ asset('js/color-modes.js') }}"></script>
+
+<!-- Libs CSS -->
+<link href="{{ asset('vendor/simplebar/simplebar.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('vendor/bootstrap-icons/font/bootstrap-icons.min.css') }}" rel="stylesheet" />
+
+<!-- Scroll Cue -->
+<link rel="stylesheet" href="{{ asset('vendor/scrollcue/scrollCue.css') }}" />
+
+<!-- Box icons -->
+<link rel="stylesheet" href="{{ asset('fonts/css/boxicons.min.css') }}" />
+
+<!-- Theme CSS -->
+<!-- build:css @@webRoot/assets/css/theme.min.css -->
+<link rel="stylesheet" href="{{ asset('css/theme.css') }}" />
+<!-- endbuild -->
---
+++ resources/views/components/scripts.blade.php
@@ -0,0 +1,11 @@
+<!-- Libs JS -->
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('vendor/simplebar/simplebar.min.js') }}"></script>
+<script src="{{ asset('vendor/headhesive/headhesive.min.js') }}"></script>
+
+<!-- Theme JS -->
+<!-- build:js @@webRoot/assets/js/theme.min.js -->
+<script src="{{ asset('js/main.js') }}"></script>
+<script src="{{ asset('js/offcanvas.js') }}"></script>
+<!-- endbuild -->
+
